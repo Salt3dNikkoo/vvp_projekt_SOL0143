54e8f2f81571aa2e120742a7625cf55dfad408da
Nikol (SOL0143) <84701769+Salt3dNikkoo@users.noreply.github.com>
2025-06-05 15:58:48 +0200

    Update __init__.py

--- fractal_lib/__init__.py
+++ fractal_lib/__init__.py
@@ -1,8 +1,9 @@
 """
-Inicializační soubor pro knihovnu fraktálů.
+Inicializační soubor pro knihovnu fraktálů (fractal_lib).
 Zpřístupňuje hlavní funkce pro generování fraktálů.
 """
 
 from .generation import generate_mandelbrot, generate_julia
+from .visualization import interactive_fractal_viewer
 
-__all__ = ["generate_mandelbrot", "generate_julia"]
+__all__ = ["generate_mandelbrot", "generate_julia", "interactive_fractal_viewer"]
